0cc552ddf21b10748bdc0402e8bcd1977aec35c3
Jakub Stefanski <code@jakubstefanski.com>
2017-02-23 16:20:09 +0100

    Transmit HTTP to RabbitMQ RPC

--- connector.js
+++ connector.js
@@ -4,9 +4,11 @@ var amqp = require('amqplib');
 var winston = require('winston');
 var uuid = require('node-uuid');
 
+var connectorLogin;
 var connectorConnection;
 var connectorChannel;
 var connectorQueue;
+var connectorRequests = {};
 
 function connect(params) {
     var connString = 'amqp://' +
@@ -14,6 +16,8 @@ function connect(params) {
         encodeURIComponent(params.hostname) + ':' + params.port + '/' +
         encodeURIComponent(params.vhost) +
         '?heartbeat=' + encodeURIComponent(params.heartbeat);
+
+    connectorLogin = params.login;
 
     var channel;
     return amqp.connect(connString).then(function (conn) {
@@ -35,42 +39,85 @@ function connect(params) {
         winston.info('Using RabbitMQ queue: %s', q.queue);
 
         connectorQueue = q.queue;
-        prepareChannel(channel, q.queue, "New connection");
+        return prepareChannel(channel, q.queue, "New connection");
     }, die);
 }
 
 function call(rpc) {
-    //rpc.requestExchange;
-    //rpc.responseExchange;
-    //rpc.body;
-    //rpc.routingKey;
-    //rpc.contentType;
-    //rpc.timeout;
+    // TODO: fix not existing Exchange error handling
+    var timestamp = new Date().getTime();
+    return new Promise(function (resolve, reject) {
+        function resolveError(status, message) {
+            resolve({
+                status: status,
+                payload: message,
+                contentType: 'text/plain'
+            });
+        }
 
-    Promise.all([
-            connectorChannel.checkExchange(rpc.requestExchange),
-            connectorChannel.checkExchange(rpc.responseExchange)
-    ]).catch(handleRequestError);
-}
+        return connectorChannel.checkExchange(rpc.requestExchange).then(function () {
+            return connectorChannel.checkExchange(rpc.responseExchange)
+        }, function () {
+            resolveError(404, 'Exchange "' + rpc.requestExchange + '" does not exist');
+        }).then(function () {
+            return connectorChannel.bindQueue(connectorQueue, rpc.responseExchange, connectorQueue);
+        }, function () {
+            resolveError(404, 'Exchange "' + rpc.responseExchange + '" does not exist');
+        }).then(function () {
+            var correlationId = uuid.v4();
+            setTimeout(function () {
+                var request = connectorRequests[correlationId];
+                if (request) {
+                    request.resolve({
+                        status: 504,
+                        payload: 'Request timed out',
+                        contentType: 'text/plain'
+                    });
+                }
+            }, rpc.timeout);
+            connectorRequests[correlationId] = {
+                resolve: resolve
+            }
 
-function handleRequestError(err) {
-    winston.warn(err.message);
+            return connectorChannel.publish(rpc.requestExchange, rpc.routingKey, rpc.body, {
+                mandatory: true,
+                replyTo: connectorQueue,
+                correlationId: correlationId,
+                contentType: rpc.contentType,
+                timestamp: timestamp,
+                type: rpc.requestExchange,
+                userId: connectorLogin
+            });
+        });
+    });
 }
 
 function prepareChannel(ch, queue, reason) {
     ch.on('error', function (error) {
         if (error.code == 404) {
             return connectorConnection.createChannel().then(function (ch) {
-                prepareChannel(ch, queue, error.message);
+                return prepareChannel(ch, queue, error.message);
             }, die);
         } else {
             die(error);
         }
     });
-    ch.consume(queue);
-    connectorChannel = ch;
 
     winston.info('Created channel, reason: %s', reason);
+    connectorChannel = ch;
+
+    return ch.consume(queue, handleResponse, {noAck: true});
+}
+
+function handleResponse(msg) {
+    var request = connectorRequests[msg.properties.correlationId];
+    if (request) {
+        request.resolve({
+            status: 200,
+            payload: msg.content,
+            contentType: msg.contentType
+        });
+    }
 }
 
 function die() {
@@ -79,7 +126,12 @@ function die() {
     } else if (arguments.length == 1) {
         winston.error(arguments[0]);
     }
-    process.exit(123);
+
+    connectorConnection.close().then(function () {
+        process.exit(123);
+    }).catch(function () {
+        process.exit(123);
+    });
 }
 
 module.exports = {
--- routes/rpc.js
+++ routes/rpc.js
@@ -1,5 +1,6 @@
 'use strict';
 
+var winston = require('winston');
 var express = require('express');
 var connector = require('../connector');
 
@@ -7,16 +8,29 @@ var router = express.Router();
 
 /* POST RPC command */
 router.post('/:command/:routing?', function(req, res) {
+    if (req.params.routing) {
+        winston.info("RPC - %s - %s", req.params.command, req.params.routing);
+    } else {
+        winston.info("RPC - %s", req.params.command);
+    }
+
+    var timeout = parseInt(req.query.timeout);
+    if (isNaN(timeout)) {
+        timeout = 20 * 1000;
+    }
+
     connector.call({
         requestExchange: req.params.command + 'Request',
         responseExchange: req.params.command + 'Response',
         body: req.body || new Buffer(""),
         routingKey: req.params.routing || "",
         contentType: req.headers['content-type'] || 'application/json',
-        timeout: req.query.timeout || 20 * 1000
+        timeout: timeout
+    }).then(function (p) {
+        res.status(p.status);
+        res.set('Content-Type', p.contentType);
+        res.send(p.payload);
     });
-
-    res.send('OK');
 });
 
 module.exports = router;
